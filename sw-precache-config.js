9bb6c860cab9712221d85e32caf5d1cd4263d452
Jack Byrne <jbyrne@glgroup.com>
2017-06-25 07:37:25 +0100

    path to service-worker

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -5,7 +5,7 @@ module.exports = {
     'build/static/**/!(*map*)'
   ],
   "runtimeCaching": [{
-    "urlPattern": /^https:\/\/pure-fjord-81149.herokuapp.com\/service-worker.js/,
+    "urlPattern": /^https:\/\/jobs.glgresearch.com/data-bowie/service-worker.js/,
     "handler": "networkFirst"
   }],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
