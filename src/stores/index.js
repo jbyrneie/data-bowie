af6ad4d129c52868a60e98afb831e7d38fc7f1a7
Jack Byrne <jbyrne@glgroup.com>
2017-06-25 09:28:13 +0100

    removed unwanted components and stores

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,13 +1,7 @@
-import AppStore from './appStore';
-import ProductsStore from './productsStore';
-import ProductStore from './productStore';
 import ServerStore from './serverStore';
 import {RouterStore} from 'mobx-router';
 
 const store = {
-  appStore: new AppStore(),
-  productStore: new ProductStore(),
-  productsStore: new ProductsStore(),
   serverStore: new ServerStore(),
   router: new RouterStore()
 };
--- src/views.js
+++ src/views.js
@@ -4,9 +4,6 @@ import React from 'react';
 import {Route} from 'mobx-router';
 
 //components
-import ProductList from './components/productList';
-import ProductDetails from './components/productDetails';
-import Iframe from './components/iframe';
 import Home from './components/home'
 
 let prefix=''
@@ -16,31 +13,13 @@ if (process.env.NODE_ENV === 'production')
 console.log('in views...', prefix);
 
 const views = {
-  home1: new Route({
-    path: `${prefix}/`,
-    component: <ProductList/>
-  }),
   home: new Route({
-    path: `${prefix}/hasselhoff`,
+    path: `${prefix}/`,
     component: <Home/>
-  }),
-  iframe: new Route({
-    path: `${prefix}/iframe`,
-    component: <Iframe/>
-  }),
-  product: new Route({
-    path: '${prefix}/product',
-    component: <ProductDetails/>,
-    beforeEnter: (route, params, store) => {
-    }
-  }),
-  products: new Route({
-    path: '${prefix}/products',
-    component: <ProductList/>
   }),
   catchall: new Route({
     path:`${prefix}/:def`,
-    component: <ProductList/>
+    component: <Home/>
   })
 };
 export default views;
