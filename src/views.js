3363fa0c90440f96ebb68f94a20d47e83ec97f81
Jack Byrne <jbyrne@glgroup.com>
2017-06-25 08:58:37 +0100

    prefix routes

--- src/views.js
+++ src/views.js
@@ -8,23 +8,32 @@ import ProductList from './components/productList';
 import ProductDetails from './components/productDetails';
 import Iframe from './components/iframe';
 
+console.log('in views...');
+let prefix=''
+if (process.env.NODE_ENV === 'production')
+  prefix = process.env.REACT_APP_DATA_BOWIE_MOUNT
+
 const views = {
   home: new Route({
-    path: '/',
+    path: `${prefix}/`,
     component: <ProductList/>
   }),
   iframe: new Route({
-    path: `/iframe`,
+    path: `${prefix}/iframe`,
     component: <Iframe/>
   }),
   product: new Route({
-    path: '/product',
+    path: '${prefix}/product',
     component: <ProductDetails/>,
     beforeEnter: (route, params, store) => {
     }
   }),
   products: new Route({
-    path: '/products',
+    path: '${prefix}/products',
+    component: <ProductList/>
+  }),
+  catchall: new Route({
+    path:`${prefix}/:def`,
     component: <ProductList/>
   })
 };
